585aab223bcab86d1946497a2c90ec8bf5c9db37
Michał <michal.czech00@gmail.com>
2020-11-01 23:39:32 +0100

    Fix port variable

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
  
 const app = new express();
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
  
 app.use(express.static('static'));
  
